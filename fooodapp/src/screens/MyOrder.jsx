c4d8a13dc2927732fd2f0ca28d74f84b28a45ad6
Rushikesh Bhalkar <98863246+Rushi12bhalkar@users.noreply.github.com>
2024-07-15 18:02:31 +0530

    Update MyOrder.jsx

--- fooodapp/src/screens/MyOrder.jsx
+++ fooodapp/src/screens/MyOrder.jsx
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myorderdata", {
+        await fetch("https://foodixpress-backend.onrender.com/api/myorderdata", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
